77e614d5b459528fd703f2c9a70669c0190292c9
Lucas <llisboaa@outlook.com>
2014-03-21 03:10:39 -0300

    Iniciando layout

---
+++ Jogo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Jogo.Models;
+
+namespace Jogo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Usuario usuario)
+        {
+            if (!(string.IsNullOrEmpty(usuario.Nome) && string.IsNullOrEmpty(usuario.Email)))
+            {
+                return View("HomeGame", usuario);
+            }
+            return View();
+        }
+
+        public ActionResult HomeGame()
+        {
+            return View();
+        }
+
+        public ActionResult Game()
+        {
+            return View();
+        }
+	}
+}
